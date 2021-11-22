2fec06bcefac5129476d1831b516d5eee377dbe5
Atishek Kumar <kumaratishek22@gmail.com>
2021-11-22 15:28:54 +0530

    Solved the circular dependecy for NeuralNet.train

--- smallnet/neural_net.py
+++ smallnet/neural_net.py
@@ -2,12 +2,6 @@ from typing import Sequence, Iterator, Tuple
 
 from smallnet.tensor import Tensor
 from smallnet.layers.layer import Layer
-from smallnet.data.data import DataIterator
-from smallnet.data.batch import Batch
-from smallnet.loss.loss import Loss
-from smallnet.loss.mean_squared import MSE
-from smallnet.optimiser.optimiser import Optimiser
-from smallnet.optimiser.stochastic_gradient_descent import SGD
 
 class NeuralNet:
     def __init__(self, network: Sequence[Layer]) -> None:
@@ -27,23 +21,7 @@ class NeuralNet:
 
     def parameters_gradients(self) -> Iterator[Tuple[Tensor, Tensor]]:
         for layer in self.network:
-            for (_, parameter), (__, gradient) in zip(layer.parameters.items(), layer.gradients.items()):
-                yield parameter, gradient
+            for name, parameter in layer.parameters.items():
+                yield parameter, layer.gradients[name]
 
-    def train(self,
-              inputs: Tensor,
-              targets: Tensor,
-              epochs: int = 4000,
-              iterator: DataIterator = Batch(),
-              loss: Loss = MSE(),
-              optimiser: Optimiser = SGD()) -> None:
-        for epoch in range(epochs):
-            iter_loss = 0.0
-            for batch in iterator(inputs, targets):
-                predicted = self.forward(batch.inputs)
-                iter_loss += loss.loss(batch.targets, predicted)
-                grads = loss.gradient(batch.targets, predicted)
-                self.backward(grads)
-                optimiser.step(self)
-            print(epoch, iter_loss)
 
---
+++ smallnet/train.py
@@ -0,0 +1,27 @@
+from smallnet.tensor import Tensor
+from smallnet.layers.layer import Layer
+from smallnet.data.data import DataIterator
+from smallnet.data.batch import Batch
+from smallnet.loss.loss import Loss
+from smallnet.loss.mean_squared import MSE
+from smallnet.optimiser.optimiser import Optimiser
+from smallnet.optimiser.stochastic_gradient_descent import SGD
+from smallnet.neural_net import NeuralNet
+
+# later move this back to neural net
+def train(network: NeuralNet,
+          inputs: Tensor,
+          targets: Tensor,
+          epochs: int = 4000,
+          iterator: DataIterator = Batch(),
+          loss: Loss = MSE(),
+          optimiser: Optimiser = SGD()) -> None:
+    for epoch in range(epochs):
+        iter_loss = 0.0
+        for batch in iterator(inputs, targets):
+            predicted = network.forward(batch.inputs)
+            iter_loss += loss.loss(batch.targets, predicted)
+            grads = loss.gradient(batch.targets, predicted)
+            network.backward(grads)
+            optimiser.step(network)
+        print(epoch, iter_loss)
